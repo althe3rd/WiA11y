afb7a2a93256b338f135b8b9cf88e6cdb71e2224
Al Nemec <al.nemec@wisc.edu>
2025-02-09 18:26:53 -0600

    more config

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -2,14 +2,17 @@ module.exports = {
   apps: [{
     name: 'wia11y',
     script: 'src/server.js',
-    env_production: {
+    env: {
       NODE_ENV: 'production',
       PORT: 3000,
       MONGODB_URI: 'mongodb+srv://wia11y_admin:ZZyTkJrL34javvag@wia11y.wpyxo.mongodb.net/?retryWrites=true&w=majority&appName=WiA11y',
       JWT_SECRET: 'p2s5v8y/B?E(H+MbQeThWmZq4t6w9z$C&F)J@NcRfUjXn2r5u8x/A%D*G-KaPdS',
       CORS_ORIGIN: 'https://wia11y.netlify.app'
     },
-    log_date_format: 'YYYY-MM-DD HH:mm:ss.SSS',
-    exp_backoff_restart_delay: 100
+    time: true,
+    autorestart: false,
+    exp_backoff_restart_delay: 100,
+    error_file: '/var/www/WiA11y/backend/logs/error.log',
+    out_file: '/var/www/WiA11y/backend/logs/out.log'
   }]
 } 
