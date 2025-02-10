95add8e6c21eae98ad1779e2f440e83c6e6bbe55
Al Nemec <al.nemec@wisc.edu>
2025-02-09 18:34:32 -0600

    updating packages

---
+++ backend/src/models/crawlModel.js
@@ -0,0 +1,11 @@
+const { getDB } = require('../config/mongodb');
+
+const collection = () => getDB().collection('crawls');
+
+module.exports = {
+  findOne: (query) => collection().findOne(query),
+  find: (query) => collection().find(query).toArray(),
+  insertOne: (doc) => collection().insertOne(doc),
+  updateOne: (query, update) => collection().updateOne(query, update),
+  deleteOne: (query) => collection().deleteOne(query)
+}; 
